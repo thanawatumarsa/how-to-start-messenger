3b95343f83f0587bdb104afc3402831887eb467e
thanawatumarsa <thanawatumarsa@gmail.com>
2016-12-11 11:09:43 +0700

    ADD Api 11

--- server.js
+++ server.js
@@ -24,20 +24,28 @@ app.post('/webhook/', function (req, res) {
     let sender = event.sender.id
     if (event.message && event.message.text) {
       let text = event.message.text
-     var location = event.message.text
-     var weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&units=metric&appid=ea5272e74853f242bc0efa9fef3dd9f3'
-     request({
-       url: weatherEndpoint,
-       json: true
-     }, function(error, response, body) {
-       try {
-         var condition = body.main;
-         sendTextMessage(sender, "Today is " + condition.temp + "Celsius in " + location);
-       } catch(err) {
-         console.error('error caught', err);
-         sendTextMessage(sender, "There was an error.");
-       }
-     })
+      var location = event.message.text
+      var weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&units=metric&appid=ea5272e74853f242bc0efa9fef3dd9f3'
+      request({
+        url: weatherEndpoint,
+        json: true
+      }, function(error, response, body) {
+        try {
+          var condition = body.main;
+          sendTextMessage(sender, "Today is " + condition.temp + "Celsius in " + location);
+        } catch(err) {
+          console.error('error caught', err);
+          sendTextMessage(sender, "There was an error.");
+        }
+      })
+
+      if (text === 'Generic') {
+        sendGenericMessage(sender)
+        continue
+      }
+      var text2 = text.split(' ')
+      sendTextMessage(sender, parseInt(text2[0]) + parseInt(text2[1]) )
+    }
     if (event.postback) {
       let text = JSON.stringify(event.postback)
       sendTextMessage(sender, 'Postback received: ' + text.substring(0, 200), token)
@@ -49,6 +57,55 @@ app.post('/webhook/', function (req, res) {
 
 function sendTextMessage (sender, text) {
   let messageData = { text: text }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: messageData
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
+
+function sendGenericMessage (sender) {
+  let messageData = {
+    'attachment': {
+      'type': 'template',
+      'payload': {
+        'template_type': 'generic',
+        'elements': [{
+          'title': 'First card',
+          'subtitle': 'Element #1 of an hscroll',
+          'image_url': 'http://messengerdemo.parseapp.com/img/rift.png',
+          'buttons': [{
+            'type': 'web_url',
+            'url': 'https://www.messenger.com',
+            'title': 'web url'
+          }, {
+            'type': 'postback',
+            'title': 'Postback',
+            'payload': 'Payload for first element in a generic bubble'
+          }]
+        }, {
+          'title': 'Second card',
+          'subtitle': 'Element #2 of an hscroll',
+          'image_url': 'http://messengerdemo.parseapp.com/img/gearvr.png',
+          'buttons': [{
+            'type': 'postback',
+            'title': 'Postback',
+            'payload': 'Payload for second element in a generic bubble'
+          }]
+        }]
+      }
+    }
+  }
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {access_token: token},

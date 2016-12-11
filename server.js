b72798f3074af05b2b8b535b2461a439788266dd
thanawatumarsa <thanawatumarsa@gmail.com>
2016-12-11 09:56:29 +0700

    Fix Bug 3

--- server.js
+++ server.js
@@ -24,11 +24,20 @@ app.post('/webhook/', function (req, res) {
     let sender = event.sender.id
     if (event.message && event.message.text) {
       let text = event.message.text
-      if (text === 'Generic') {
-        sendGenericMessage(sender)
-        continue
-      }
-      sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
+
+      if (text)
+        switch (text) {
+          case 'hi' :
+          case 'Hi' :
+          case 'hello' :
+          case 'Hello' :
+          case 'สวัสดี' :
+          case 'หวัดดี' :
+          case 'ดีจ้า' :
+          sendTextMessage (sender, "HI")
+            break;
+          default:sendTextMessage (sender, "What")
+        }
     }
     if (event.postback) {
       let text = JSON.stringify(event.postback)

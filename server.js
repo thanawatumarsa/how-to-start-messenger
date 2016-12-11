4bad3445a7f2e7f73c0c92434d56a6ebe38e8687
thanawatumarsa <thanawatumarsa@gmail.com>
2016-12-11 11:07:45 +0700

    ADD Api 10

--- server.js
+++ server.js
@@ -23,20 +23,21 @@ app.post('/webhook/', function (req, res) {
     let event = req.body.entry[0].messaging[i]
     let sender = event.sender.id
     if (event.message && event.message.text) {
-      var text = event.message.text
-      var nameCity = 'http://api.openweathermap.org/data/2.5/weather?q=' +text+ '&units=metric&appid=1dbb2e0928332cda13bbefb9104d13e4'
-      request({
-        url: nameCity,
-        json: true
-      }, function(error, response, body) {
-        try {
-          var condition = body.main;
-          sendTextMessage(sender, "วันนี้ที่ " + text + " มีอุณหภูมิที่ " + condition.temp + " องศาเซลเซียส ")
-        } catch(err) {
-          console.error('error caught', err);
-          sendTextMessage(sender, "ไม่พบชื่อเมืองที่ค้นหา กรุณากรอกชื่อเมืองอีกครั้ง");
-        }
-      })
+      let text = event.message.text
+     var location = event.message.text
+     var weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&units=metric&appid=ea5272e74853f242bc0efa9fef3dd9f3'
+     request({
+       url: weatherEndpoint,
+       json: true
+     }, function(error, response, body) {
+       try {
+         var condition = body.main;
+         sendTextMessage(sender, "Today is " + condition.temp + "Celsius in " + location);
+       } catch(err) {
+         console.error('error caught', err);
+         sendTextMessage(sender, "There was an error.");
+       }
+     })
     if (event.postback) {
       let text = JSON.stringify(event.postback)
       sendTextMessage(sender, 'Postback received: ' + text.substring(0, 200), token)
